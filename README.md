a0ab878e7ee4a01da4e6b7e4b90c6f9214df12eb
Calum Forster <calum.forster@cgi.com>
2017-04-28 10:16:57 +0100

    Added documentation

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
-# GraphQL example application
+# GraphQL example application using GitHub's API
 
 ## How to run
-  1. clone this repo
-  2. ```npm install```
-  3. ``` npm start```
+  0. Download and install Node.js
+    * [Using a packet manager (apt, yum, brew, ...)](https://nodejs.org/en/download/package-manager/)
+    * [Windows & Mac/Linux without package manager](https://nodejs.org/en/download/)
+  1. Using the terminal, clone this repo and cd into the directory
+  2. ```$ npm install```
+  3. ```$ npm start```
+  4. The app should be running at http://localhost:4000/graphql
+
+_Note: due to the GitHub API rate limit you can only make 60 requests per hour_
+
+### Example query
+To test the app is working correctly try the following query:
+```js
+query{
+  profile(username:"Bob"){
+    name
+    username
+    following{
+      name
+    }
+  }
+}
+```
+
+![gif](/extra/query_example.gif)
+
+## How it works
+The application is built using [GraphQL](https://www.npmjs.com/package/graphql) and [express-graphql](https://www.npmjs.com/package/express-graphql). The documentation for GraphQL is really good and you should read the following pages if you get stuck:
+  * [Getting Started](http://graphql.org/graphql-js/)
+  * [Running an Express GraphQL Server](http://graphql.org/graphql-js/running-an-express-graphql-server/)
+  * [Object Types](http://graphql.org/graphql-js/object-types/)
+  * [Basic Types](http://graphql.org/graphql-js/basic-types/)
+
+Each page should take around 10-15 minutes to read through.
+
+This app also does a lot with Promises. If you are unfamiliar with these then spending some time reading the documentation would be very beneficial. Promises are used to keep track of asynchronous functions.
+  * [Promises](https://www.promisejs.org/)
+
+The NPM module [request](https://www.npmjs.com/package/request) is used to make the HTTP GET requests to the GitHub API.
+
+
+## Want to learn more?
+Here are some tasks you can try which will help your understanding of GraphQL.
+
+  1. Add a User field for `created_at` which will show the date as a string of when the profile was created. _[EASY]_
+  2. Add a User field for `followers` and also add a instance method in the User class to resolve this field. This should resolve to a field similar to the `following` field. _[HARD]_
+  3. Add a new Object Type for repositories and add a field for `repos` to the User object. See the GitHub API for repos [here](https://developer.github.com/v3/repos/). You will need to do this with `https` which may not work behind a proxy. _Hint: try ```curl https://api.github.com/users/facebook/repos```_ _[VERY HARD]_
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var graphqlHTTP = require('express-graphql')
 var { buildSchema } = require('graphql')
-
 var request = require('request')
+var github = 'http://api.github.com/users/'
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -17,43 +17,70 @@ var schema = buildSchema(`
   }
 `)
 
-var github = 'http://api.github.com/users/'
+// The class for a User type. The resolvers are instance methods
 class User {
+  /**
+  * The constructor is called whenever the GraphQL Query profile(username)
+  * is called
+  * It resolves the fields: username, name, url
+  **/
   constructor(username) {
+
+    // Set the username
     this.username = username
 
+    // Create the url needed to fetch this users data from GitHub
     var user_element_url = github+this.username
+
+    // Save the context for this user so we can use it in the Promise below
     var thisUser = this
 
+    // return a Promise so that GraphQL waits for the response from GitHub
+    // before resolving the fields
     return new Promise( (fulfill, reject) => {
       // query the Github API:
       console.log('GET REQUEST: '+ user_element_url)
-      request.get({url: user_element_url, headers: {'User-Agent':'request'}}, function (err, response, body) {
+
+      // The GitHub API requires the User-Agent header be set
+      request.get({url: user_element_url, headers: {'User-Agent':'request'}}, (err, response, body) => {
 
         if (err) {
+          // We reject the Promsie if there is an error
           return reject(err)
         }
 
+        // Parse the JSON response and save the required data to the User object
         var profile = JSON.parse(body)
-        thisUser.following_url = profile.following_url
-        console.log(thisUser.following_url)
-        thisUser.following_count = profile.following
+
+        // The name and url are provided
         thisUser.name = profile.name
         thisUser.url = profile.html_url
 
+        // the following_url is required by the following() method
+        thisUser.following_url = profile.following_url
+
+        // we can now fulfill the Promise and return the User object
         fulfill(thisUser)
       })
     })
   }
 
+  /**
+  * The following method is called whenever the
+  * User field 'following' is asked for
+  **/
   following() {
-    console.log(this.following_url)
-    var collection_url = this.following_url.replace('https', 'http').replace('{/other_user}','')
-    var following_count = this.following_count
 
+    // Set the url for fetching the list of profiles this user is following
+    // replace https with http, and remove the '{/other_user}' from the end of the url
+    var collection_url = this.following_url.replace('https', 'http').replace('{/other_user}','')
+
+    // return a Promise so that GraphQL waits for each HTTP request to complete
+    // before resolving
     return new Promise( (fulfill, reject) => {
+
       console.log('GET REQUEST: '+ collection_url)
-      request.get({url: collection_url,headers: {'User-Agent':'request'}}, function (err, response, body) {
+      request.get({url: collection_url,headers: {'User-Agent':'request'}}, (err, response, body) => {
 
         if (err) {
           return reject(err)
@@ -64,8 +91,10 @@ class User {
 
         var promises = []
 
+        // create a Promise for each following profile
         following_profiles.forEach( function (profile) {
 
+          // create a Promise to resolve each following profile
           var new_promise = new Promise( (fulfill,reject) => {
             new User(profile.login).then( (new_profile) => {
 
@@ -75,11 +104,14 @@ class User {
             })
           })
 
+          // Add each promise to the promises array
           promises.push(new_promise)
 
         })
 
+        // Run all promises simultaneously
         Promise.all(promises).then( () => {
+          // After all promises have finished, return the following array
           return fulfill(following)
         })
 
@@ -96,7 +128,10 @@ var root = {
   }
 }
 
+// Start the HTTP server using Express
 var app = express()
+
+// Use the graphqlHTTP middleware
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
@@ -108,5 +143,18 @@ app.use('/graphql', graphqlHTTP({
     path: error.path
   })
 }))
-app.listen(4000)
-console.log('Running a GraphQL API server at localhost:4000/graphql')
+
+// Listen on port 4000
+app.listen(4000, () => {
+  console.log('Running a GraphQL API server at localhost:4000/graphql')
+
+  // Get the GitHub rate limit and log to the console
+  request.get({ url: 'http://api.github.com/rate_limit', headers: {'User-Agent':'request'}}, (err, response, body) => {
+    body = JSON.parse(body)
+    var resetTime = new Date(body.rate.reset*1000 - Date.now())
+    console.log('GitHub API Requests:')
+    console.log(' - limit: ' + body.rate.limit)
+    console.log(' - remaining: ' + body.rate.remaining)
+    console.log(' - resets in: ' + (resetTime.getMinutes()) + ' minutes ')
+  })
+})
